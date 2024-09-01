c201938fac38158ee446e022e0a7186ea893b082
PhoenixFeder <phoenixfeder5633@gmail.com>
2024-09-01 13:30:11 +0200

    java + zip utils added

--- configuration.nix
+++ configuration.nix
@@ -112,6 +112,10 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
+    jdk
+    zip
+    gzip
+    gnutar
   #  wget
     kitty
     rofi-wayland
